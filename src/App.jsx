6b5e475378c641fc8015eefc7614e3de9fee18fe
FelipeCostaq <felipecostasiqu@gmail.com>
2026-01-25 20:25:21 -0300

    :art: added logic LoadingScreen to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import './styles/global.css'
+import "./styles/global.css";
+import { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
+
+//Components
+import Navbar from "./components/layout/Navbar";
+import LoadingScreen from "./components/layout/LoadingScreen";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
     <>
-      <div>
+      <div className="relative min-h-screen">
+        <AnimatePresence>
+          {isLoading && (
+            <LoadingScreen onComplete={() => setIsLoading(false)} />
+          )}
+        </AnimatePresence>
+
+        {!isLoading && (
+          <>
+            <div className="scanlines" />
+            <Navbar />
+            <main></main>
+          </>
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
